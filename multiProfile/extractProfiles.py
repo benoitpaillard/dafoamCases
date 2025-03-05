57cb6f7f377b5c896d58844195f4978434d34949
jony <ben.paillard@gmail.com>
2025-03-05 08:43:02 +0100

    lower speed

--- multiProfile/extractProfiles.py
+++ multiProfile/extractProfiles.py
@@ -1,7 +1,12 @@
 App.newDocument()
-
+import os
 import Mesh,Part,Draft
 from FreeCAD import Base
+
+meshDTC=Mesh.Mesh("/home/jony/dafoamCases/multiProfile/DTC-scaled.stl")
+
+shapeDTC = Part.Shape()
+shapeDTC.makeShapeFromMesh(meshDTC.Topology, 0.1,False)
 
 mesh1=Mesh.Mesh(u"/home/jony/dafoamCases/multiProfile/profile0.stl")
 
@@ -12,9 +17,12 @@ shape1.translate(Base.Vector(-0.5,0,0))
 
 edges1=[x for x in shape1.Edges if x.CenterOfGravity.z<0.01]
 
-wire1=Part.Wire(edges1)
+wire1=Part.Wire(Part.sortEdges(edges1)[0])
 
 Part.show(wire1,'wire1')
+
+face1=Part.makeFace([wire1],'Part::FaceMakerSimple')
+Part.show(face1)
 
 wire1_top = Draft.make_clone(FreeCAD.ActiveDocument.getObject("wire1"))
 
@@ -26,7 +34,8 @@ sail1=Part.makeRuledSurface(wire1,wire1_top.Shape)
 
 Part.show(sail1)
 
-
+face1_top=Part.makeFace([wire1_top.Shape],'Part::FaceMakerSimple')
+Part.show(face1_top)
 
 mesh2=Mesh.Mesh(u"/home/jony/dafoamCases/multiProfile/profile1.stl")
 
@@ -52,9 +61,12 @@ Part.show(furler)
 
 
 
-wire2=Part.Wire(edges2)
+wire2=Part.Wire(Part.sortEdges(edges2)[0])
 
 Part.show(wire2,'wire2')
+
+face2=Part.makeFace([wire2],'Part::FaceMakerSimple')
+Part.show(face2)
 
 wire2_top=wire2.copy()
 wire2_top.scale(.4,Base.Vector(0,0,0))
@@ -70,8 +82,12 @@ sail2=Part.makeRuledSurface(wire2,wire2_top)
 
 Part.show(sail2)
 
+face2_top=Part.makeFace([wire2_top],'Part::FaceMakerSimple')
+Part.show(face2_top)
 
-sail=Part.makeCompound([sail1,sail2])
-sail.scale(0.5)
+sail=Part.makeCompound([sail1,sail2,face1,face1_top,face2,face2_top,furler])
+sail.scale(.5)
 
 Part.show(sail)
+
+sail.exportStl('patch0.stl')
